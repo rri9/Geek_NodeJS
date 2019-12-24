96dc951c6e325fab1cb6299d9fe145ade94b3274
rri9 <rri9@mail.ru>
2019-12-24 14:32:43 +0300

    lesson-1

--- ansi.js
+++ ansi.js
@@ -3,16 +3,10 @@ const cursor = ansi(process.stdout);
 
 cursor.beep();
 
-
-// cursor.bg.grey().write("   ").write("\n");
-// // cursor.eraseLine();
-// cursor.horizontalAbsolute(5).write("5");
-// cursor.goto(10, 5).write("10, 5");
-
-function cursorPrint(str, color, bgColor, x, y, ) {
-  cursor.goto(x, y).red().bg.green().write(str);
-  cursor.reset();
-}
+cursor.bg.grey().write("   ").write("\n");
+// cursor.eraseLine();
+cursor.horizontalAbsolute(5).write("5");
+cursor.goto(10, 5).write("*");
 
 // cursor
 //   .bg.hex("#ff0000")  //red
@@ -20,19 +14,10 @@ function cursorPrint(str, color, bgColor, x, y, ) {
 //   .bg.hex("#FF4500")  //orange
 //   .write("     ")
 //   .reset();
+// displaying wrong hex colors!
 
-// cursorPrint("hello", "red", "green", 5, 2);
-
-// cursor.hex('#FF0000').write("#FF0000");
-// cursor.hex('#00FF00').write("#00FF00");
-
-let col = "grey()";
-// `cursor.bg.${col}.write('123')`;
-// `${cursor.bg.grey().write('123').bg.red().write('456')}`;
-let str = `${cursor.bg.grey().write('123').bg.red().write('456')}`;
-console.log(str);
-str += ".bg.blue().write('789')";
-console.log(str);
-str;
+cursor.write("\n")
+cursor.hex('#FF0000').write("#FF0000");
+cursor.hex('#00FF00').write("#00FF00");
 
 cursor.reset();
