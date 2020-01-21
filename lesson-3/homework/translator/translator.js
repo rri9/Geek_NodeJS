c05a1d8d1771465c597c816c6e8af94c4631351c
rri9 <rri9@mail.ru>
2020-01-21 18:11:04 +0300

    + translator

--- lesson-3/homework/translator/translator.js
+++ lesson-3/homework/translator/translator.js
@@ -4,56 +4,45 @@ const urlUtils = require("url");
 
 const yaKey = "trnsl.1.1.20200121T040642Z.e9eb334bc1ded363.d7f5ecfb10168d32b6d1eeeae9829b472f4718d7";
 
-// const req = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20200121T040642Z.e9eb334bc1ded363.d7f5ecfb10168d32b6d1eeeae9829b472f4718d7&text=Hello&lang=ru&format=plain&options=1";
-// console.log(urlUtils.parse(req));
-
-// console.log(argv);
-// console.log( argv._[3].replace("", ) );
-if(argv._[2]===undefined || argv._[3]===undefined) {
+if (argv._[2] === undefined || argv._[3] === undefined) {
   console.log("Error: Script parameters are absent.");
   console.log('Basic usage:\ntranslator.js ru "Hello, World!"');
   process.exit();
 }
-if(argv._[4]) {
+if (argv._[4]) {
   console.log("Error: Too many script parameters.");
   console.log('Basic usage:\ntranslator.js ru "Hello, World!"');
   process.exit();
 }
 
 let urlObj = {
-protocol: 'https:',
-hostname: 'translate.yandex.net',
-pathname: '/api/v1.5/tr.json/translate',
+  protocol: "https:",
+  hostname: "translate.yandex.net",
+  pathname: "/api/v1.5/tr.json/translate",
   query: {
     key: yaKey,
-    // text: encodeURI(argv._[3]),
     text: argv._[3],
-    text: argv._[4],
     lang: argv._[2],
     format: "plain",
     options: 1,
-  }
+  },
 };
-console.log( urlUtils.format(urlObj) );
 
-
-request({
-  //request params
-  method: "POST",
-  uri: urlUtils.format(urlObj),
-  // form: {
-  //   key: yaKey,
-  // },
-}, (err, response, data) => {
-    //api answer stuff
+request(
+  {
+    method: "POST",
+    uri: urlUtils.format(urlObj),
+  },
+  (err, response, data) => {
     if (err) {
       console.log(err);
     } else {
       const ans = JSON.parse(data);
-      if(response.statusCode === 200 && ans.code === 200) {
+      if (response.statusCode === 200 && ans.code === 200) {
         ans.text.forEach(text => {
           console.log(text);
         });
       }
     }
-});
+  }
+);
