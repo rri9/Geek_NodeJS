e31e77354eeb9c568f73300756fab44f53755125
rri9 <rri9@mail.ru>
2020-01-24 07:32:18 +0300

    + getFilteredNews method

--- lesson-4/homework/news/news.js
+++ lesson-4/homework/news/news.js
@@ -18,10 +18,14 @@ app.all("/", (req, res, next) => {
 });
 
 app.get("/news", (req, res) => {
-  console.log("Receved request at /news");
-  getNews(url).then(newsObj => {
-    res.render("news", newsObj);
-  });
+  console.log("Receved GET request at /news");
+  getNews(url)
+    .then(newsObj => {
+      getFilterNews(newsObj, req.query);
+    })
+    .then(filteredNewsObj => {
+      res.render("news", filteredNewsObj);
+    });
 });
 
 app.listen(8000, () => {
@@ -44,8 +48,11 @@ function getNews() {
               date: el.children[3].children[0].data,
               title: el.children[1].children[0].data,
               text: el.children[7].children[0].children[0].data.replace(/&#34;/g, '"'),
+              category: el.children[9].children[0].data,
+              link: el.children[5].children[0].data,
             };
             newsObj.news.push(news_item);
+            // debugger;
           });
           resolve(newsObj);
         }
@@ -54,6 +61,30 @@ function getNews() {
   });
 }
 
+function getFilterNews(newsObj, query) {
+  return new Promise((resolve, reject) => {
+    if (!query.newsAmount) {
+      query.newsAmount = 10;
+    }
+    if (!query.category) {
+      query.category = "all";
+    }
+
+    const filteredNewsObj = {
+      news: [],
+    };
+    filteredNewsObj.news = newsObj.news.filter(el => {
+      // debugger;
+      if (el.category === query.category) {
+        return true;
+      }
+    });
+    debugger;
+    console.log(filteredNewsObj);
+
+    resolve(filteredNewsObj);
+  });
+}
 // let newsObj = {
 //   news: [
 //     {
