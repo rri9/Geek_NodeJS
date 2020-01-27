682586a115df7cd0d1ecff4e26ca3f2fea48eb89
rri9 <rri9@mail.ru>
2020-01-27 07:47:15 +0300

    + cookies

--- lesson-4/homework/news/news.js
+++ lesson-4/homework/news/news.js
@@ -1,18 +1,26 @@
 const express = require("express"),
   app = express(),
   path = require("path"),
-  templating = require("consolidate"); //Поддержка шаблонизаторов
+  // templating = require("consolidate"), //Поддержка шаблонизаторов
+  cookieParser = require('cookie-parser'),
+  hbs = require('hbs'); //Заменяет consolidate и делает ненужной строку app.engine("hbs", templating.handlebars);
 
 const request = require("request"),
   cheerio = require("cheerio"),
   url = "https://rss.newsru.com/top/main/";
+let categories = ['all', 'world', 'russia', 'sport'];
 
-app.engine("hbs", templating.handlebars); //Выбираем функцию шаблонизации для hbs
+hbs.registerHelper('isSelected', (category, key) => {
+  return category === key ? 'selected' : '';
+});
+
+// app.engine("hbs", templating.handlebars); //Выбираем функцию шаблонизации для hbs
 app.set("view engine", "hbs"); //По умолчанию используем шаблоны hbs
 app.set("views", path.resolve(__dirname, "views")); //Каталог с шаблонами
 
 app.use('/styles', express.static(path.resolve(__dirname, 'assets')) );
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.all("/", (req, res, next) => {
   console.log("Receved request at /");
@@ -22,17 +30,39 @@ app.all("/", (req, res, next) => {
 
 app.get("/news", (req, res) => {
   console.log("Receved GET request at /news");
-  res.render('news');
+  let newsObj = {
+    cookies: {
+      category: 'all',
+      newsAmount: 5,
+    },
+    categoriesList: categories,
+  };
+  if (isObjEmpty(req.cookies)) {
+    newsObj.cookies = {
+      category: req.cookies.category,
+      newsAmount: req.cookies.newsAmount,
+    }
+  }
+  if (isObjEmpty(req.query)) {  //Приоритет передаваемым параметрам get-запроса
+    if (req.query.category) {
+      newsObj.cookies.category = req.query.category;
+    }
+    if (req.query.newsAmount) {
+      newsObj.cookies.newsAmount = req.query.newsAmount;
+    }
+  }
+  res.render('news', newsObj);
 });
 
 app.post("/news", (req, res) => {
   console.log("Receved POST request at /news");
-  debugger
   getNews(url)
     .then(newsObj => {
       return getFilteredNews(newsObj, req.body);
     })
     .then(filteredNewsObj => {
+      res.cookie('category', filteredNewsObj.cookies.category, {maxAge: 1000*60*60*24*1, });
+      res.cookie('newsAmount', filteredNewsObj.cookies.newsAmount, {maxAge: 1000*60*60*24*1, });
       res.render("news", filteredNewsObj);
     });
 });
@@ -62,6 +92,7 @@ function getNews() {
             };
             newsObj.news.push(news_item);
           });
+          renewNewsCategory(newsObj);
           resolve(newsObj);
         }
       }
@@ -72,6 +103,11 @@ function getNews() {
 function getFilteredNews(newsObj, data) {
     const filteredNewsObj = {
       news: [],
+      cookies: {
+        newsAmount: data.newsAmount,
+        category: data.category,
+      },
+      categoriesList: categories,
     };
 
   for (let i = 0, counter = 0; i < newsObj.news.length && counter < +data.newsAmount; i++) {
@@ -81,4 +117,20 @@ function getFilteredNews(newsObj, data) {
     }
   }
   return filteredNewsObj;
+}
+
+function isObjEmpty(obj) {
+  if (Object.keys(obj).length > 0) {
+    return true;
+  }
+  return false;
+}
+
+///////
+function renewNewsCategory(newsObj) {
+  newsObj.news.forEach(news => {
+    if (!categories.includes(news.category)) {
+      categories.push(news.category);
+    }
+  });
 }
