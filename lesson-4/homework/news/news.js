2b900e5f5eca72043178b3be24d5bee1a484f9ae
rri9 <rri9@mail.ru>
2020-01-29 07:55:27 +0300

    fix modules folders + add eslint + fix eslin error

--- lesson-4/homework/news/news.js
+++ lesson-4/homework/news/news.js
@@ -1,36 +1,35 @@
-const express = require("express"),
+const express = require('express'),
   app = express(),
-  path = require("path"),
+  path = require('path'),
   // templating = require("consolidate"), //Поддержка шаблонизаторов
   cookieParser = require('cookie-parser'),
+  // eslint-disable-next-line spaced-comment
   hbs = require('hbs'); //Заменяет consolidate и делает ненужной строку app.engine("hbs", templating.handlebars);
 
-const request = require("request"),
-  cheerio = require("cheerio"),
-  url = "https://rss.newsru.com/top/main/";
-let categories = ['all', 'world', 'russia', 'sport'];
+const request = require('request'),
+  cheerio = require('cheerio'),
+  url = 'https://rss.newsru.com/top/main/';
+const categories = ['all', 'world', 'russia', 'sport'];
 
-hbs.registerHelper('isSelected', (category, key) => {
-  return category === key ? 'selected' : '';
-});
+hbs.registerHelper('isSelected', (category, key) => (category === key ? 'selected' : ''));
 
 // app.engine("hbs", templating.handlebars); //Выбираем функцию шаблонизации для hbs
-app.set("view engine", "hbs"); //По умолчанию используем шаблоны hbs
-app.set("views", path.resolve(__dirname, "views")); //Каталог с шаблонами
+app.set('view engine', 'hbs'); //По умолчанию используем шаблоны hbs
+app.set('views', path.resolve(__dirname, 'views')); //Каталог с шаблонами
 
-app.use('/styles', express.static(path.resolve(__dirname, 'assets')) );
+app.use('/styles', express.static(path.resolve(__dirname, 'assets')));
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.all("/", (req, res, next) => {
-  console.log("Receved request at /");
-  res.send("Use ./news");
+app.all('/', (req, res, next) => {
+  console.log('Receved request at /');
+  res.send('Use ./news');
   next();
 });
 
-app.get("/news", (req, res) => {
-  console.log("Receved GET request at /news");
-  let newsObj = {
+app.get('/news', (req, res) => {
+  console.log('Receved GET request at /news');
+  const newsObj = {
     cookies: {
       category: 'all',
       newsAmount: 5,
@@ -41,9 +40,10 @@ app.get("/news", (req, res) => {
     newsObj.cookies = {
       category: req.cookies.category,
       newsAmount: req.cookies.newsAmount,
-    }
+    };
   }
-  if (isObjEmpty(req.query)) {  //Приоритет передаваемым параметрам get-запроса
+  if (isObjEmpty(req.query)) {
+    //Приоритет передаваемым параметрам get-запроса
     if (req.query.category) {
       newsObj.cookies.category = req.query.category;
     }
@@ -54,21 +54,24 @@ app.get("/news", (req, res) => {
   res.render('news', newsObj);
 });
 
-app.post("/news", (req, res) => {
-  console.log("Receved POST request at /news");
+app.post('/news', (req, res) => {
+  console.log('Receved POST request at /news');
   getNews(url)
-    .then(newsObj => {
-      return getFilteredNews(newsObj, req.body);
-    })
+    .then(newsObj => getFilteredNews(newsObj, req.body))
     .then(filteredNewsObj => {
-      res.cookie('category', filteredNewsObj.cookies.category, {maxAge: 1000*60*60*24*1, });
-      res.cookie('newsAmount', filteredNewsObj.cookies.newsAmount, {maxAge: 1000*60*60*24*1, });
-      res.render("news", filteredNewsObj);
+      res.cookie('category', filteredNewsObj.cookies.category, { maxAge: 1000 * 60 * 60 * 24 * 1 });
+      res.cookie('newsAmount', filteredNewsObj.cookies.newsAmount, {
+        maxAge: 1000 * 60 * 60 * 24 * 1,
+      });
+      res.render('news', filteredNewsObj);
+    })
+    .catch(err => {
+      console.log(err);
     });
 });
 
 app.listen(8000, () => {
-  console.log("Server listenning on port 8000...");
+  console.log('Server listenning on port 8000...');
 });
 
 function getNews() {
@@ -76,42 +79,41 @@ function getNews() {
     request(url, (err, response, body) => {
       if (err) {
         console.log(`Request to ${url} error: ${err}`);
-      } else {
-        if (response.statusCode === 200) {
-          let newsObj = {
-            news: [],
+        reject(err);
+      } else if (response.statusCode === 200) {
+        const newsObj = {
+          news: [],
+        };
+        const $ = cheerio.load(body, { xmlMode: true });
+        $('item').each((i, el) => {
+          const newsItem = {
+            date: el.children[3].children[0].data,
+            title: el.children[1].children[0].data,
+            text: el.children[7].children[0].children[0].data.replace(/&#34;/g, '"'),
+            category: el.children[9].children[0].data,
+            link: el.children[5].children[0].data,
           };
-          const $ = cheerio.load(body, { xmlMode: true });
-          $("item").each((i, el) => {
-            const news_item = {
-              date: el.children[3].children[0].data,
-              title: el.children[1].children[0].data,
-              text: el.children[7].children[0].children[0].data.replace(/&#34;/g, '"'),
-              category: el.children[9].children[0].data,
-              link: el.children[5].children[0].data,
-            };
-            newsObj.news.push(news_item);
-          });
-          renewNewsCategory(newsObj);
-          resolve(newsObj);
-        }
+          newsObj.news.push(newsItem);
+        });
+        renewNewsCategory(newsObj);
+        resolve(newsObj);
       }
     });
   });
 }
 
 function getFilteredNews(newsObj, data) {
-    const filteredNewsObj = {
-      news: [],
-      cookies: {
-        newsAmount: data.newsAmount,
-        category: data.category,
-      },
-      categoriesList: categories,
-    };
+  const filteredNewsObj = {
+    news: [],
+    cookies: {
+      newsAmount: data.newsAmount,
+      category: data.category,
+    },
+    categoriesList: categories,
+  };
 
   for (let i = 0, counter = 0; i < newsObj.news.length && counter < +data.newsAmount; i++) {
-    if (data.category === "all" || newsObj.news[i].category === data.category) {
+    if (data.category === 'all' || newsObj.news[i].category === data.category) {
       filteredNewsObj.news.push(newsObj.news[i]);
       counter++;
     }
