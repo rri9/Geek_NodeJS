c9bc75a8c7b735031886ca73ddb290eeacb9fe67
rri9 <rri9@mail.ru>
2020-01-31 07:42:36 +0300

    + add MySQL connection

--- lesson-5/lecture/test.js
+++ lesson-5/lecture/test.js
@@ -1,10 +1,34 @@
-const mysql = require();
-
-const connection = mysql.createConnection({
-  host:     'localhost',
-  database: 'testDB',
-});
-
-//TODO Протестировать соединение с БД MySQL
 //TODO Протестировать соединение с БД MongoDB
 //TODO Сохранение изменений в БД в Docker'е
+
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: '192.168.99.100',
+  port: '3307',
+  database: 'mysql',
+  user:     'root',
+  password: 'pwd',
+});
+
+connection.connect(err => {
+  if (err) {
+    return console.log('Ошибка: ' + err);
+  }
+  console.log('Успешное подключение к БД');
+  connection.end();
+  return console.log('Подключение закрыто');
+});
+
+// const pool = mysql.createPool({
+//   connectionLimit: 5,
+//   host:     'localhost',
+//   database: 'testDB',
+//   user:     'root',
+//   password: 'pwd',
+// });
+// pool.end((err) => {
+//   if (err) {
+//     console.log(err.message);
+//   }
+// });
