bbe4ed10a34ab8d4367a34303901fed33f3bc6f4
rri9 <rri9@mail.ru>
2020-02-04 07:47:05 +0300

    fix error handling

--- lesson-5/homework/src/main.js
+++ lesson-5/homework/src/main.js
@@ -1,13 +1,41 @@
-const Conn = require('./config');
+//TODO Pool
+
+const express = require('express'),
+  app = express(),
+  path = require('path'),
+  hbs = require('hbs');
+
+const Conn = require('./connection');
 const conn = new Conn();
 const newData = ['BMW', '725', 2017, 2100000];
 
-conn.createConn();
-conn.execCmd('USE lessonDB;');
-// conn.execCmdData('INSERT INTO cars VALUES(?,?,?,?);', newData);
-conn.execCmd('SELECT * FROM `cars` WHERE `id` = ?', ['2'])
-// conn.execCmd('SELECT * FROM cars;')
-  .catch((err) => {
-    throw err;
-  })
-  .then(() => conn.closeConn() );
+// conn.execCmd('USE homeworkDB;').catch(err => console.log(err.message));
+conn.execCmd('USE homeworkDB;').catch(err => {
+  if (err) {
+    conn._initDB()
+      .finally(() => conn.closeConn())
+      .catch(err => console.log('Инициализация БД не выполнена!'))
+      .then(() => conn.execCmd('USE homeworkDB;'));
+  }
+});
+// conn._initDB()
+//   .then(() => this.closeConn())
+//   .catch(err => {
+//     throw err;
+//   });
+
+conn
+  .execCmdData('INSERT INTO cars (mark, model, year, price) VALUES (?,?,?,?);', newData)
+  // conn.execCmdData('INSERT INTO cars VALUES ("BMW","725",2017,2100000);')
+  //   // .finally(() => conn.closeConn())
+  //   .catch(err => console.log(err.message));
+
+  // conn.execCmdData('SELECT * FROM cars WHERE id = ?;', ['2'])
+  //   // .finally(() => conn.closeConn())
+  //   .catch(err => console.log(err.message));
+
+  // conn.execCmd('SELECT * FROM cars;')
+  .then(result => console.log(result))
+  .finally(() => conn.closeConn())
+  // .catch(err => { throw err; });
+  .catch(() => console.log('Запрос к БД не выполнен!'));
