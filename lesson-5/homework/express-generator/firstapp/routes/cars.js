04dab94bd64e8a2b96fa6c1d9ee93a8fa53b308e
rri9 <rri9@mail.ru>
2020-02-06 07:36:02 +0300

    + edit form

--- lesson-5/homework/express-generator/firstapp/routes/cars.js
+++ lesson-5/homework/express-generator/firstapp/routes/cars.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const connection = require('../lib/db');
 
-/* GET users listing. */
-router.get('/', (req, res, next) => {
+/* GET cars */
+router.get('/', (req, res) => {
   connection.query('SELECT * FROM cars ORDER BY id asc', (err, rows) => {
     if (err) {
       console.log(err);
@@ -13,17 +13,16 @@ router.get('/', (req, res, next) => {
         'error.stack': err.stack,
       });
     } else {
-      debugger
       res.render('cars', { cars: rows });
     }
   });
 });
-
-router.get('/add', (req, res, next) => {
+/* ADD cars form  */
+router.get('/add', (req, res) => {
   res.render('addCar', {});
 });
-
-router.post('/add', (req, res, next) => {
+/* ADD cars method*/
+router.post('/add', (req, res) => {
   const car = {
     mark: req.body.mark,
     model: req.body.model,
@@ -31,7 +30,7 @@ router.post('/add', (req, res, next) => {
     price: req.body.price,
   };
   console.log(car);
-  connection.query('INSERT cars SET ?', car, (err, result) => {
+  connection.query('INSERT cars SET ?', car, (err) => {
     if (err) {
       console.log('error in post /add');
       res.render('error', {
@@ -40,10 +39,28 @@ router.post('/add', (req, res, next) => {
         'error.stack': err.stack,
       });
     } else {
-      console.log('redirection...');
       res.redirect('/cars');
     }
   });
+});
+/* EDIT car form */
+router.get('/edit', (req, res) => {
+  if (Object.prototype.hasOwnProperty.call(req.query, 'id')) {
+    connection.query('SELECT * FROM cars WHERE id = ?', req.query.id, (err, rows) => {
+      if (err) {
+        console.log(err);
+        res.render('error', {
+          message: err.message,
+          'error.status': err.status,
+          'error.stack': err.stack,
+        });
+      } else {
+        res.render('editCar', { car: rows[0] });
+      }
+    });
+  } else {
+    res.render('editCar');
+  }
 });
 
 module.exports = router;
